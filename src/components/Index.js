2c0d98a2a25213cb66e2d530e87dc7a732b30797
Ashutosh Kumar <53196209+TiwariAshuism@users.noreply.github.com>
2022-08-31 18:53:25 +0530

    Project setup

---
+++ src/components/Index.js
@@ -0,0 +1,16 @@
+import Billing from "./Billing";
+import  Navbar from "./Navbar";
+import  Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import FeedbackCard from "./FeedbackCard";
+import Testimonials from "./Testimonials";
+
+
+export {Navbar,Billing,Business,Button,CardDeal,Clients,FeedbackCard,CTA,Footer,GetStarted,Hero,Stats,Testimonials}
